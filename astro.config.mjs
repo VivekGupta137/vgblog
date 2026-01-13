0eff23c281808d84cdc5e54e1fca7b83052ce59e
vivek <vivekgpt107@gmail.com>
2026-01-13 23:08:00 +0530

    added loadenc

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,10 +22,13 @@ import starlightMarkdownBlocks, { Aside } from "starlight-markdown-blocks";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 import starlightFullViewMode from "starlight-fullview-mode";
 import rehypeGitHubBadgeLinks from "./src/lib/rehype-github-badge-links";
+import { loadEnv } from "vite";
+
+const env = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.PUBLIC_DOMAIN || "http://localhost:4321/",
+  site: env.PUBLIC_DOMAIN || "http://localhost:4321/",
   
   markdown: {
     remarkPlugins: [remarkMath],
@@ -51,10 +54,10 @@ export default defineConfig({
         starlightThemeRapide(),
         starlightImageZoom(),
         starlightGiscus({
-          repo: process.env.PUBLIC_GISCUS_REPO || "",
-          repoId: process.env.PUBLIC_GISCUS_REPO_ID || "",
-          category: process.env.PUBLIC_GISCUS_CATEGORY || "",
-          categoryId: process.env.PUBLIC_GISCUS_CATEGORY_ID || "",
+          repo: env.PUBLIC_GISCUS_REPO || "",
+          repoId: env.PUBLIC_GISCUS_REPO_ID || "",
+          category: env.PUBLIC_GISCUS_CATEGORY || "",
+          categoryId: env.PUBLIC_GISCUS_CATEGORY_ID || "",
           mapping: "pathname",
           reactions: true,
           inputPosition: "top",
@@ -62,7 +65,7 @@ export default defineConfig({
           theme: "preferred_color_scheme"
         }),
         starlightPageActions({
-          baseUrl: process.env.PUBLIC_DOMAIN || "",
+          baseUrl: env.PUBLIC_DOMAIN || "",
           prompt: "Read {url} and explain its main points briefly."
         }),
         starlightMarkdownBlocks({
@@ -131,25 +134,15 @@ export default defineConfig({
             directory: "connect",
           },
         },
-        {
-          label: "Resources",
-          items: [
-            // An external link to the NASA website opening in a new tab.
-            {
-              label: "NASA",
-              link: "https://www.nasa.gov/",
-              attrs: { target: "_blank", style: "font-style: italic" },
-            },
-          ],
-        },
+        
       ],
     }),
     plantuml({
-      serverUrl: process.env.PUBLIC_PLANTUML_SERVER_URL || "http://localhost:8080/png/",
+      serverUrl: env.PUBLIC_PLANTUML_SERVER_URL || "https://www.plantuml.com/plantuml/png/",
       addWrapperClasses: true,
     }),
     d2({
-      skipGeneration: process.env.NODE_ENV === "production" || process.env.D2_SKIP_GENERATION === "true",
+      skipGeneration: env.NODE_ENV === "production" || env.D2_SKIP_GENERATION === "true",
     }),
   ],
 
